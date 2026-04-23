61e64c710c501ad30c4061b9e3a53d16b44bdc48
sofi450 <karen.diaz07@usa.edu.co>
2026-04-23 07:30:32 -0500

    Rename constant ANIO_ACTUAL to AÑO_ACTUAL

--- ConstantesNomina.java
+++ ConstantesNomina.java
@@ -1,4 +1,4 @@
 public class ConstantesNomina {
-    public static final int ANIO_ACTUAL = 2024;
+    public static final int AÑO_ACTUAL = 2024;
     public static final double TASA_PRESTACIONES = 0.12;
 }
